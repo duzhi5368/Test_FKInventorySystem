72d6b274d76241d7856406cec4099e6fa1ca9622
FreeKnight <duzhi5368@gmail.com>
2023-08-20 18:52:47 -0500

    add quest system

---
+++ Assets/FKGame/Scripts/TaskSystem/Runtime/Quest/QuestDatabase.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FKGame.QuestSystem
+{
+	[System.Serializable]
+	public class QuestDatabase : ScriptableObject
+	{
+		public List<Quest> items = new List<Quest>();
+		public List<Settings> settings = new List<Settings>();
+	}
+}
